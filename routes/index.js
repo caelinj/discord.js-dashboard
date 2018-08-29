9653403e717962e781df951c0912c75e11ea3143
caelinj <caelinj@yandex.com>
2018-08-29 13:25:46 +1000

    Basic, Discord-inspired UI

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('index', { user: req.session.user || null });
+    res.render('index', { pageTitle: 'Dashboard', user: req.session.user || null });
 });
 
 module.exports = router;
