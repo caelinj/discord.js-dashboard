dffa1c4d0d28a2cd435a06da0e98b7c25cf13250
caelinj <caelinj@yandex.com>
2018-08-29 10:42:49 +1000

    Sessions, nearly completed oauth2 flow

--- app.js
+++ app.js
@@ -4,7 +4,24 @@ const app = express();
 let port = require('./config.json').port || 3000;
 app.set('port', port);
 
+const session = require('express-session');
+
 app.set('view engine', 'ejs');
+app.use(session({
+    genid: (req) => {
+        return genuuid();
+    },
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        path: '/',
+        httpOnly: true,
+        secure: false,
+        maxAge: 604800000,
+    },
+    expires: 604800000,
+}));
 require('./router')(app);
 
 app.listen(port, () => console.info(`Listening on port ${port}`));
--- routes/discord.js
+++ routes/discord.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const { clientId, clientSecret, scopes, redirectUri } = require('../config.json');
+const fetch = require('node-fetch');
 
 router.get('/', (req, res) => {
     const authorizeUrl = `https://discordapp.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${scopes.join('%20')}`;
@@ -11,7 +12,21 @@ router.get('/callback', (req, res) => {
     const accessCode = req.query.code;
     if (!accessCode) throw new Error('No access code returned frm Discord');
 
-    res.send(`Success, got an access code.`);
+    fetch({
+        method: 'POST',
+        body: {
+            client_id: clientId,
+            client_secret: clientSecret,
+            grant_type: 'authorization_code',
+            redirect_uri: redirectUri,
+            scope: scopes.join('%20'),
+            code: accessCode,
+        },
+    })
+    .then(res => res.json())
+    .then(response => {
+        req.session.DiscordAuth = response;
+    });
 });
 
 module.exports = router;
